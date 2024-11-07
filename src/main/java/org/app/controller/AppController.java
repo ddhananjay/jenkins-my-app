89756dc4d8ac5186b0b61a5b074b8731e8303d68
dhananjay dumbre <dhananjayd510@gmail.com>
2024-11-06 19:59:45 -0600

    added kubenetes deployment

--- src/main/java/org/app/controller/AppController.java
+++ src/main/java/org/app/controller/AppController.java
@@ -3,10 +3,7 @@ package org.app.controller;
 import org.app.entity.PortalUser;
 import org.app.service.AppService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/app")
@@ -15,7 +12,7 @@ public class AppController {
     @Autowired
     private AppService appService;
 
-    @RequestMapping("/data/{username}")
+    @RequestMapping(method = RequestMethod.GET, path = "/data/{username}")
     public String getData(@PathVariable String username) {
         PortalUser user = appService.getUserInfo(username);
         if(user != null) {
